c980af87be7fdc93f090b53ac5cba1f3ec5fc1f1
Joshua <holt.josh@gmail.com>
2009-02-05 20:47:38 -0500

    Changed README after announcing on the #sproutcore channel

---
+++ public/static/familytracker/en/_cache/controllers/contact_list-1233883348.js
@@ -0,0 +1,27 @@
+/* Start ----------------------------------------------------- controllers/contact_list.js*/
+
+// ==========================================================================
+// Familytracker.ContactListController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.CollectionController
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+Familytracker.contactListController = SC.CollectionController.create(
+/** @scope Familytracker.contactListController */ {
+  //useControllersForContent: NO,
+  commitChangesImmediately: false,
+  contentBinding: 'Familytracker.contactDetailController.contacts'
+}) ;
+
+
+/* End ------------------------------------------------------- controllers/contact_list.js*/
+
